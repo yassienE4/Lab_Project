aa34f83e545131e5e4db6bd89d99bded0078a332
yassienE4 <yassienebrahim@gmail.com>
2024-11-20 11:06:13 +0200

    login register main ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    loginwindow *l = new loginwindow();
+    this->hide();
+    l->show();
+}
+
+
+void MainWindow::on_pushButton_Regiter_clicked()
+{
+    registerwindow *r = new registerwindow();
+    this->hide();
+    r->show();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include "loginwindow.h"
+#include "registerwindow.h"
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -16,6 +17,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_Login_clicked();
+
+    void on_pushButton_Regiter_clicked();
 
 private:
     Ui::MainWindow *ui;
