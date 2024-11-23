49fc7be68de325c661a4570f218a76413e2e7e7d
SeifRamy <Seifeldine@aucegypt.edu>
2024-11-24 00:27:59 +0200

    item.cpp

--- item.cpp
+++ item.cpp
@@ -1,5 +1,65 @@
 #include "item.h"
+#include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-item::item() {
+
+// Constructor
+item::item(string n, double p, int d, string desc, bool a, int rp) {
+    name = n;
+    price = p;
+    demand = d;
+    description = desc;
+    available = a;
+    rentalPeriod = rp;
+}
+
+bool item::isavailable() {
+    return available;
+}
+
+void item::updateprice(double newPrice) {
+    price = newPrice;
+}
+
+void item::updatepricedemand(double multiplier) {
+    price *= (1 + multiplier * demand);
+}
+
+void item::updatename(const string& newName) {
+    name = newName;
+}
+
+void item::updatedescription(const string& newDescription) {
+    description = newDescription;
+}
+
+void item::updaterental(int newRentalPeriod) {
+    rentalPeriod = newRentalPeriod;
+}
+
+void item::updateavailability(bool isAvailable) {
+    available = isAvailable;
+}
+
+void item::printItem() {
+    cout << "Name: " << name << ", Price: " << price << ", Demand: " << demand
+         << ", Description: " << description << ", Available: " << (available ? "Yes" : "No")
+         << ", Rental Period: " << rentalPeriod << " days" << endl;
+}
+
+bool item::operator==(const item& other)  {
+    return name == other.name && price == other.price && demand == other.demand;
+}
+
+vector<pair<int, item>> Qlist;
+void removeitem(const item& x) {
+    auto it = Qlist.begin();
+    while (it != Qlist.end()) {
+        if (it->second == x) {
+            it = Qlist.erase(it);
+        } else {
+            ++it;
+        }
+    }
 }
--- item.h
+++ item.h
@@ -8,16 +8,22 @@ private:
     string name;
     double price;
     int demand;
+    string description;
+    bool available;
+    int rentalPeriod;
 public:
     item();
+    item(string name, double price, int demand, string description = "", bool available = true, int rentalPeriod = 0);
     bool isavailable();
-    void updateprice();
-    void updatepricedemand();
-    void updatename();
-    void updatedescription();
-    void updaterental();
-    void updateavailability();
-    void removeitem(item x);
+    void updateprice(double Update);
+    void updatepricedemand(double m);
+    void updatename(const string& Nn);
+    void updatedescription(const string& Nd);
+    void updaterental(int ur);
+    void updateavailability(bool Av);
+    void printItem();
+    void removeItem(item x);
+    bool operator==(const item& other);
 };
 
 #endif // ITEM_H
