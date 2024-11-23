6a40166518660ebbaa2f12c86308d64c91238434
Fady5aad <fadysaad@aucegypt.edu>
2024-11-23 21:11:34 +0200

    more changes

--- centerflow.cpp
+++ centerflow.cpp
@@ -10,7 +10,7 @@ centerflow::centerflow()
 {
     MainWindow w;
     w.show();
-    QList<allaccounts> *accounts;
+    Qlist<users> *normalUsers;
     int size;
     fstream myfile;
     myfile.open(":/files/accountfile.txt",ios::in);
@@ -22,11 +22,60 @@ centerflow::centerflow()
         for(int i = 0; i < size; i++)
         {
             users t;
-            allaccounts *temp;
-            t = temp;
-            getline(myfile,temp.account.name);
-            getline(myfile,temp.password);
-            accounts.push_back(t);
+            getline(myfile,t.name);
+            getline(myfile,t.password);
+            normalUsers.push_back(t);
         }
         myfile.close();
+    }
+
+    Qlist<admin> *adminUser;
+    int adminsize;
+    fstream myfile;
+    myfile.open(":/files/admin.txt",ios::in);
+    if(myfile.is_open())
+    {
+        string tem;
+        getline(myfile,tem);
+        size = stoi(tem);
+        for(int i = 0; i < adminsize; i++)
+        {
+            admin t;
+            getline(myfile,t.name);
+            getline(myfile,t.password);
+            adminUser.push_back(t);
+        }
+        myfile.close();
+    }
+
+}
+
+centerflow::~centerflow()
+{
+    myfile.open(":/files/accountfile.txt",ios::out);
+
+    if(myfile.is_open())
+    {
+        myfile<<size + "\n";
+        for (int i = 0; i < size; i++)
+        {
+            myfile<< normalUsers[i].name + "\n";
+            myfile<< normalUsers[i].passwords + "\n";
+        }
+        myfile.close();
+    }
+
+    myfile.open(":/files/admin.txt",ios::out);
+
+    if(myfile.is_open())
+    {
+        myfile<<adminsize + "\n";
+        for (int i = 0; i < adminsize; i++)
+        {
+            myfile<< adminUser[i].name + "\n";
+            myfile<< adminUser[i].passwords + "\n";
+        }
+        myfile.close();
+    }
+
 }
