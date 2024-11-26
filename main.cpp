2922668356527cd4a2cf6330cde6cf73e3ee56ed
yassienE4 <yassienebrahim@gmail.com>
2024-11-26 04:04:36 +0200

    .

--- main.cpp
+++ main.cpp
@@ -10,25 +10,6 @@ int main(int argc, char *argv[])
     MainWindow w(nullptr, &x);
     w.show();
 
-    //
-    // 1. NO STRINGS ONLY QSTRING
-    //
-    // 2.filename objname(nullptr, &center);
-    // 2.mainwindow w(nullptr)
-    // 2.w->show()
-    //
-    // 3. center.getvectoruser();
-
- /*   if(clicked button left)
-        i--
-    if(clicked right)
-        i++
-
-        array[i];
-    for()
-
-
 
     return a.exec();
-*/
 }
